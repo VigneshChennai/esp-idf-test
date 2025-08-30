30afef0759c6b48a96eee60b50021f5f5c735955
Vigneshwaran P <vigneshchennai@live.in>
2025-08-31 03:33:52 +0530

    Added tokio support

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,125 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use std::convert::TryInto;
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+use anyhow::Result;
+use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
 
-    let mut i = 0;
+use esp_idf_svc::eventloop::EspSystemEventLoop;
+use esp_idf_svc::hal::modem::Modem;
+use esp_idf_svc::hal::peripherals::Peripherals;
+use esp_idf_svc::log::EspLogger;
+use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::sys::{
+    esp, esp_vfs_eventfd_config_t, esp_vfs_eventfd_register
+};
+use esp_idf_svc::timer::EspTaskTimerService;
+use esp_idf_svc::wifi::{AsyncWifi, EspWifi};
+
+use log::info;
+
+
+const SSID: &str = "NETGEAR13";
+const PASSWORD: &str = "royalphoenix978";
+
+
+fn config_eventfd() -> Result<(), esp_idf_svc::sys::EspError> {
+    // Register the eventfd VFS driver to allow async operations
+    // This is needed for tokio to work.
+    let config = esp_vfs_eventfd_config_t {
+        max_fds: 1,
+        ..Default::default()
+    };
+    esp! { unsafe { esp_vfs_eventfd_register(&config) } }
+}
+
+fn print_memory_info() {
+    use esp_idf_svc::sys::{
+        esp_get_free_heap_size, esp_get_minimum_free_heap_size,
+        heap_caps_get_total_size, MALLOC_CAP_8BIT
+    };
+    // We need unsafe here as we are directly using function from esp-idf-sys crate.
+    // which is a direct binding to the ESP-IDF C API.
+    //
+    // This is safe as long as we ensure that the ESP-IDF C API is used correctly.
+    unsafe {
+        // Get the total heap size available to the application.
+        let total_heap = heap_caps_get_total_size(MALLOC_CAP_8BIT) as u32;
+        info!("Total heap size: {} bytes", total_heap);
+
+        // Get the current free heap size.
+        let free_heap = esp_get_free_heap_size();
+        info!("Current free heap size: {} bytes", free_heap);
+
+        // Get the minimum free heap size that has been observed since
+        // the application started. This is a good indicator of
+        // worst-case memory usage.
+        let min_free_heap = esp_get_minimum_free_heap_size();
+        info!("Minimum free heap size: {} bytes", min_free_heap);
+
+        // Example of a simple memory usage calculation
+        let used_heap = total_heap - free_heap;
+        info!("Currently used heap size: {} bytes", used_heap);
+    }
+}
+
+fn main() -> Result<()> {
+    esp_idf_svc::sys::link_patches(); // Required for compatibility
+    EspLogger::initialize_default();  // Enable logging
+
+    config_eventfd()?;
+
+    // Run the async main function
+    tokio::runtime::Builder::new_current_thread()
+      .enable_all()
+      .build()?
+      .block_on(async_main())?;
+
+    Ok(())
+}
+
+async fn async_main() -> Result<()> {
+    // Take required peripherals
+    let peripherals = Peripherals::take()?;
+    let modem: Modem = peripherals.modem;
+    let sysloop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+    let timer = EspTaskTimerService::new()?;
+
+    // Create the ESP WiFi driver
+    let wifi_driver = EspWifi::new(modem, sysloop.clone(), Some(nvs))?;
+
+    // Wrap it in AsyncWifi
+    let mut wifi = AsyncWifi::wrap(wifi_driver, sysloop, timer)?;
+
+    // Wi-Fi Configuration
+    let config = Configuration::Client(ClientConfiguration {
+        ssid: SSID.try_into().map_err(|_| anyhow::Error::msg("Error in SSID"))?,
+        password: PASSWORD.try_into().map_err(|_| anyhow::Error::msg("Error in Password"))?,
+        auth_method: AuthMethod::WPA2Personal,
+        ..Default::default()
+    });
+
+    // Set config and start
+    wifi.set_configuration(&config)?;
+    info!("Wi-Fi configuration set.");
+
+    wifi.start().await?;
+    info!("Wi-Fi started.");
+
+    wifi.connect().await?;
+    info!("Wi-Fi connecting...");
+
+    wifi.wait_netif_up().await?;
+    info!("Wi-Fi connected!");
 
     loop {
-        log::info!("Hello, world! : {}", i);
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        i += 1;
+        let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+        info!("IP Info: {:?}", ip_info);
+        print_memory_info();
+        // Leaking memory to testing 
+        // 1. if memory tracking working as expected
+        // 2. what happens on OOM
+        let _ = "New string object".to_owned().leak();
+        tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+
     }
-    
 }
